a8951c7de61360c28692033ce564a1cc8bbf73e6
jianjun.hu <jianjun.hu@sumscope.com>
2026-04-12 10:40:44 +0800

    feat: add dialog style

--- src/auth/utils/open-dialog.ts
+++ src/auth/utils/open-dialog.ts
@@ -1,3 +1,5 @@
+import md5 from 'md5';
+import { loginUrl } from '../config';
 import styles from './auth.css?inline';
 
 
@@ -38,6 +40,35 @@ const openDialog = () => {
 
   // 打开弹窗
   dialog.showModal();
+
+  // 监听表单提交事件
+  dialog.querySelector('form')?.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const username = (dialog.querySelector('#username') as HTMLInputElement)?.value;
+    const password = (dialog.querySelector('#password') as HTMLInputElement)?.value;
+    console.log('Username:', username);
+    console.log('Password:', password);
+    fetch(loginUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password: md5(password) })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Login successful:', data);
+      if (data.status === 200) {
+        console.log('Login successful!');
+        window.location.reload();
+      }
+      // 这里可以根据返回的数据进行相应的处理，比如存储 token 等
+    })
+    .catch(error => {
+      console.error('Login failed:', error);
+    });
+    dialog.close();
+  });
 
 };
 
