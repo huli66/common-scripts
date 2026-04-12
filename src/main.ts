5f3872d76c60295b213cfa40e26dbf90a4399cdf
huli <2969054528@qq.com>
2026-04-12 14:31:38 +0800

    feat: add fetch middleware

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
+console.log("main.ts start");
 const a = 1;
 
-const userUrl = '/node/api/user';
-const cnexUrl = '/node/api/k8s/qbweb-fim/line/cnex-emo';
+const userUrl = "/node/api/user";
+const cnexUrl = "/node/api/k8s/qbweb-fim/line/cnex-emo";
 
-console.log('1');
+console.log("1", performance.now());
 console.log(a);
 
 fetch(cnexUrl, {
-  method: 'GET',
+  method: "GET",
   headers: {
-    'Content-Type': 'application/json'
-  }
-}).then(response => response.json()).then(data => {
-  console.log('cnex data', data);
-});
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    document.querySelector("#app")!.textContent = "1222";
+    console.log("cnex data", data, performance.now());
+  });
+
+console.log("main.ts end");
